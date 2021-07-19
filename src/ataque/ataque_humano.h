593e160955818a2cef80cf2927c44e49edd3d5f7
Franco Mazzaro <franmazzaro00@gmail.com>
2021-07-19 19:45:00 -0300

    errores comunes

--- src/ataque/ataque_humano.h
+++ src/ataque/ataque_humano.h
@@ -11,15 +11,43 @@ class Ataque_humano : public Ataque{
 
     Ataque_humano(Humano *personaje);
     ~Ataque_humano();
+
     void atacar(Casilla *casilla);
+
+    //PRE:
+    //POST:
     string eleccion_arma();
+
+    //PRE:
+    //POST:
     bool tiene_arma(string arma_elegida);
+
+    //PRE:
+    //POST:
     int buscar_personaje(Casilla *casilla_a_atacar, string personaje);
+
+    //PRE:
+    //POST:
     bool tiene_balas(int cantidad_minima_balas);
+
+    //PRE:
+    //POST:
     bool validacion_ataque(Casilla *casilla_a_atacar, string arma_elegida);
+
+    //PRE:
+    //POST:
     void bajar_cantidad_objeto(int cantidad_gastada, string arma);
+
+    //PRE:
+    //POST:
     int indice_personaje(string personaje, Casilla* casilla);
+
+    //PRE:
+    //POST:
     void bajar_vida(Casilla* casilla);
+
+    //PRE:
+    //POST:
     char devolver_inicial(string objeto);
 
 };
