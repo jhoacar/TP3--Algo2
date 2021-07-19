fd86eaa5aec62f8809ec28fea8dd15ca84ed6448
Franco Mazzaro <franmazzaro00@gmail.com>
2021-07-19 16:59:49 -0300

    modificacion metodo energia_suficiente

--- src/ataque/Ataque.h
+++ src/ataque/Ataque.h
@@ -16,7 +16,7 @@ public:
     ~Ataque();
     virtual void atacar(Casilla *casilla) = 0;
     bool validacion_rango_ataque(Casilla *casilla_a_atacar, int max_rango);
-    bool energia_suficiente();
+    bool energia_suficiente(int minimo);
     void consumir_energia(int cantidad);
     int calcular_vida_con_armadura(int valor_ataque);
     int calcular_porcentaje_fuerza(int fuerza, int porcentaje);
