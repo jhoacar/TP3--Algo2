11e2ca1ab54a1b74b5697c5e05e986c10499cb0d
Agustin <asanchezd@fi.uba.ar>
2021-07-22 19:27:57 -0300

    Se actualizo rama

--- tests/test_gotoxy.cpp
+++ tests/test_gotoxy.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main(){
     limpiar_pantalla();
-    gotoxy(20,20);
-    cout<<"Esto es una prueba";
+    int matriz[5][5];
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++){
+            gotoxy(10+j*5, i+5);
+            cin >> matriz[i][j];
+
+        }
+
+    cout << endl;
+
     pausa();
 }
