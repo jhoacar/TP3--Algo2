9699dcddfdba4ce95c276a1d82ba27c5a850321c
Alejandro Toledo <toledoar86@gmail.com>
2021-07-22 20:38:31 -0300

    Defensa de Vampirella agregada.

--- src/objetos/seres/monstruos/vampiros/Vampirella.cpp
+++ src/objetos/seres/monstruos/vampiros/Vampirella.cpp
@@ -3,10 +3,11 @@
 using std::cout;
 using std::endl;
 Vampirella::Vampirella():Vampiro(){
-
+    this->murcielago= false;
 }
 Vampirella::Vampirella(Casilla *casilla,char nombre,string ID):Vampiro(casilla,nombre,ID)
 {
+    this->murcielago=true;
 }
 void Vampirella::mostrar(){
     cout<<"Objeto: Vampirella"<<endl;
@@ -16,8 +17,22 @@ Vampirella::~Vampirella()
 }
 
 void Vampirella::defender(){
-    
+    this->energia -=5;
+    this->convertir_en_murcielago();
 }
+
+void Vampirella::convertir_en_murcielago() {
+    this->murcielago = true;
+}
+
+void Vampirella::convertir_en_vampiro() {
+    this->murcielago = false;
+}
+
+bool Vampirella::es_murcielago() {
+    return this->murcielago;
+}
+
 void Vampirella::atacar(Casilla *casilla){
 
 }
--- src/objetos/seres/monstruos/vampiros/Vampirella.h
+++ src/objetos/seres/monstruos/vampiros/Vampirella.h
@@ -5,13 +5,32 @@
 
 class Vampirella : public Vampiro
 {
+    private:
+        bool murcielago;
 	public:
 		Vampirella();
 		Vampirella(Casilla *casilla,char nombre,string ID);
 		void mostrar();
 		void regenerar_energia();
 		~Vampirella();
+
+		// PRE: Vampirella ha decidido defenderse.
+        // POS: Consume 5 puntos de energía y se vuelve murcielago por un turno.
+        //      No puede ser atacada por estacas, ni agua bendita por un turno.
 		void defender();
+
+		// PRE: Vampirella se defiende.
+		// POS: Se convierte en murcielago, por lo tanto establece murcielago en true.
+		void convertir_en_murcielago();
+
+		// PRE: Vampirella se ha defendido en el turno anterior, por lo tanto muricelago es true.
+        // POS: Vampirella se vuelve a convertir en vampiro, por lo tanto se establece murcielago en false.
+		void convertir_en_vampiro();
+
+		// PRE: -
+		// POS: Devuelve true si es murcielago, caso contrario devuelve false.
+		bool es_murcielago();
+
 		void atacar(Casilla *casilla);
 	protected:
 };
