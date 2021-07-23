ff31f0a873b81b33f8370e9382e1d12cb4e95a8f
Alejandro Toledo <toledoar86@gmail.com>
2021-07-23 14:13:06 -0300

    Defensa de Vanesa agregada. Faltan métodos a implementar.

--- src/objetos/seres/humanos/cazador/Vanessa.cpp
+++ src/objetos/seres/humanos/cazador/Vanessa.cpp
@@ -15,8 +15,30 @@ Vanessa::~Vanessa()
 {
 	
 }
-void Vanessa::defender(){
 
+void Vanessa::defender(){
+    this->energia -=10;
+    if ( !this->tengo_agua_bendita() && !this->tengo_cruz() ){
+        this->vida +=10;
+    }
+    else {
+        if ( this->tengo_cruz() ){
+            cout << "Lógica de protegerse con una cruz." << endl;
+        }
+        if ( this->tengo_agua_bendita() ){
+            cout << "Lógica de proteger aliados si tiene agua y consumirla." << endl;
+        }
+    }
+}
+
+bool Vanessa::tengo_agua_bendita() {
+    objeto_referencia->asignar_nombre(NOMBRES_CHAR[AGUA]);
+    return inventario.existe(objeto_referencia,comparacion_por_nombre);
+}
+
+bool Vanessa::tengo_cruz() {
+    objeto_referencia->asignar_nombre(NOMBRES_CHAR[CRUZ]);
+    return inventario.existe(objeto_referencia,comparacion_por_nombre);
 }
 
 void Vanessa::atacar(Casilla *casilla){
--- src/objetos/seres/humanos/cazador/Vanessa.h
+++ src/objetos/seres/humanos/cazador/Vanessa.h
@@ -11,7 +11,26 @@ class Vanessa : public Cazador
 		void mostrar();
 		void regenerar_energia();
 		~Vanessa();
+
+        // PRE: Vampirella ha decidido defenderse.
+        // POS: Vampirella se defiende. Es decir:
+        //      Consume -10 puntos de energía.
+        //      Si no tiene agua bendita o una cruz, se cura +10 puntos de vida.
+        //      Si tiene agua bendita en su inventario, evita que aliados a dos casilleros de
+        //      distancia se conviertan en zombies.
+        //      Si tiene una cruz, se protege de cualquier ataque vampirico.
 		void defender();
+
+        // Comprueba si hay agua bendita en el inventario
+        // PRE: -
+        // POS: Devuelve true si tiene agua bendita en su inventario, caso contrario devuelve false.
+        bool tengo_agua_bendita();
+
+        // Comprueba si hay una cruz en el inventario
+        // PRE: -
+        // POS: Devuelve true si tiene cruz en su inventario, caso contrario devuelve false.
+        bool tengo_cruz();
+        
 		void atacar(Casilla *casilla);
 		
 	protected:
