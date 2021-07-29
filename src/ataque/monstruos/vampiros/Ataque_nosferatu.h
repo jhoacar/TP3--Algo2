ca95d30829ea716f33b150e0418ce5611fd68b15
franmazzaro <86503603+franmazzaro@users.noreply.github.com>
2021-07-29 10:07:22 -0300

    Update Ataque_nosferatu.h

--- src/ataque/monstruos/vampiros/Ataque_nosferatu.h
+++ src/ataque/monstruos/vampiros/Ataque_nosferatu.h
@@ -8,6 +8,9 @@
 class Ataque_nosferatu:public Ataque_vampiro{
     
     virtual void consumir_energia();
+    
+    //PRE: recibe el humano a convertir
+    //POST: le cambia el nombre, y le cambia su ataque y defensa a los de tipo vampiro
     void convertir_vampiro(Humano *humano);
     virtual bool tiene_energia();
     virtual bool esta_en_rango_ataque(Coordenada posicion,char arma=NO_ARMA);
